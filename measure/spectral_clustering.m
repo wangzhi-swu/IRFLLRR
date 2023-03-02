d2c58703b52cd0216581e537907b794a1e7bf10b
Zhi Wang（王智） <114987494+wangzhi-swu@users.noreply.github.com>
2023-03-02 16:49:23 +0800

    Add files via upload

---
+++ measure/spectral_clustering.m
@@ -0,0 +1,9 @@
+function [ ids ] = spectral_clustering(W, k)
+D = diag(1./sqrt(sum(W, 2)));
+W = D * W * D;
+[U, s, V] = svd(W);
+V = U(:, 1 : k);
+V = normr(V);
+% ids = kmeans(V, k, 'start','sample','maxiter', 1000,'replicates',100,'EmptyAction','singleton');
+ids = kmeans(V, k, 'emptyaction', 'singleton', 'replicates', 100, 'display', 'off');
+end
